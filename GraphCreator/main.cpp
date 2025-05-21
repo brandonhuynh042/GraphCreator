0eb40627eec4daf1d8e07ccad65fa193033dcaba
U-BSD\456223 <456223@SHS-GYBF6X3.beaverton.k12.or.us>
2025-05-21 12:37:26 -0700

    added removes, fixed print formatting

--- GraphCreator/main.cpp
+++ GraphCreator/main.cpp
@@ -8,7 +8,6 @@ int main() {
   char input[30];
   int adjacency[20][20];
   int vertices[20];
-  int weights[20];
   for (int a = 0; a < 20; a++) {
     vertices[a] = 0;
     for (int b = 0; b < 20; b++) {
@@ -47,15 +46,33 @@ int main() {
       char choiceInput[10];
       cin >> choiceInput;
       if (strcmp(choiceInput, "EDGE") == 0) {
-	cout << "What label would you like to remove?" << endl;
+	cout << "First vertice?" << endl;
+        int vOne;
+        cin >> vOne;
+        cout << "Second vertice?" << endl;
+        int vTwo;
+        cin >> vTwo;
+        adjacency[vOne-1][vTwo-1] = 0;
+        adjacency[vTwo-1][vOne-1] = 0;
       }
       if (strcmp(choiceInput, "VERTICE") == 0) {
-	
+	cout << "What label would you like to remove?" << endl;
+        int delVert;
+        cin >> delVert;
+        vertices[delVert - 1] = 0;
+        for (int a = 0; a < 20; a++) {
+          if (a = delVert - 1) {
+            for (int b = 0; b < 20; b++) {
+              adjacency[a][b] = 0;
+              adjacency[b][a] = 0;
+            }
+	    break;
+          }
+        }
       }
-
     }
     else if (strcmp(input, "PRINT") == 0) {
-      cout << " ";
+      cout << " " << " ";
       for (int i = 0; i < 20; i++) {
 	if (vertices[i] != 0) {
 	  cout << " " << vertices[i];
@@ -64,10 +81,16 @@ int main() {
       cout << '\n';
       for (int j = 0; j < 20; j++) {
 	if (vertices[j] != 0) {
+	  if (j < 10) {
+	    cout << " ";
+	  }
 	  cout << vertices[j] << " ";
 	  for (int k = 0; k < 20; k++) {
 	    if (vertices[k] != 0) {
 	      cout << adjacency[j][k] << " ";
+	      if (k > 9) {
+		cout << " ";
+	      }
 	    }
 	  }
         cout << '\n';
