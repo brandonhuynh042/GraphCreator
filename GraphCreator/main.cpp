f0c93635ec138c7921f017cca4dd59c9e7a0075b
U-BSD\456223 <456223@SHS-GYBF6X3.beaverton.k12.or.us>
2025-05-23 13:24:01 -0700

    almost

--- GraphCreator/main.cpp
+++ GraphCreator/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <cstring>
-
+#include <vector>
+#include <algorithm>
+#include "queue.h"
 using namespace std;
+
+int dijkstraPath(int source, int destination, vector<int> &visited, int adjacency[][20]);
 
 int main() {
   bool quit = false;
   char input[30];
   int adjacency[20][20];
   int vertices[20];
+  // initialize empty vertices and adj table
   for (int a = 0; a < 20; a++) {
     vertices[a] = 0;
     for (int b = 0; b < 20; b++) {
@@ -15,9 +20,10 @@ int main() {
     }
   }
   do {
-    cout << "You can: \'ADD\' \'REMOVE\' \'PRINT\' \'QUIT\'" << endl;
+    cout << "You can: \'ADD\' \'REMOVE\' \'PRINT\' \'PATHFIND\' \'QUIT\'" << endl;
     cin >> input;
     if (strcmp(input, "ADD") == 0) {
+      // which one?
       cout << "You can add: \'EDGE\' \'VERTICE\'" << endl;
       char choiceInput[10];
       cin >> choiceInput;
@@ -25,9 +31,11 @@ int main() {
 	cout << "Please label the vertice with a number 1-20: " << endl;
 	int newLabel;
 	cin >> newLabel;
+	// add vertice to user input...
 	vertices[newLabel-1] = newLabel;
       }
       if (strcmp(choiceInput, "EDGE") == 0) {
+	// take connection & weight
 	cout << "First vertice?" << endl;
 	int vOne;
 	cin >> vOne;
@@ -37,8 +45,8 @@ int main() {
         cout << "Weight?" << endl;
 	int weight;
 	cin >> weight;
+	// set adjacency table accordingly
 	adjacency[vOne-1][vTwo-1] = weight;
-        adjacency[vTwo-1][vOne-1] = weight;
       }
     }
     else if (strcmp(input, "REMOVE") == 0) {
@@ -46,20 +54,23 @@ int main() {
       char choiceInput[10];
       cin >> choiceInput;
       if (strcmp(choiceInput, "EDGE") == 0) {
+	// which connection?
 	cout << "First vertice?" << endl;
         int vOne;
         cin >> vOne;
         cout << "Second vertice?" << endl;
         int vTwo;
         cin >> vTwo;
+	// delete in adj. table
         adjacency[vOne-1][vTwo-1] = 0;
-        adjacency[vTwo-1][vOne-1] = 0;
       }
       if (strcmp(choiceInput, "VERTICE") == 0) {
 	cout << "What label would you like to remove?" << endl;
         int delVert;
         cin >> delVert;
+	// delete the vertice itsself
         vertices[delVert - 1] = 0;
+	// delete all potential links it may have had
         for (int a = 0; a < 20; a++) {
           if (a = delVert - 1) {
             for (int b = 0; b < 20; b++) {
@@ -74,11 +85,13 @@ int main() {
     else if (strcmp(input, "PRINT") == 0) {
       cout << " " << " ";
       for (int i = 0; i < 20; i++) {
+	// only showing the ones that exist
 	if (vertices[i] != 0) {
 	  cout << " " << vertices[i];
 	}
       }
       cout << '\n';
+      // print out the adjacency table for rows/columns which there are actually vertices
       for (int j = 0; j < 20; j++) {
 	if (vertices[j] != 0) {
 	  if (j < 10) {
@@ -103,9 +116,54 @@ int main() {
 	}
      }
     }
+    else if (strcmp(input, "PATHFIND") == 0) {
+       // which connection?
+        cout << "First vertice?" << endl;
+        int vOne;
+        cin >> vOne;
+        cout << "Second vertice?" << endl;
+        int vTwo;
+        cin >> vTwo;
+	vector<int> visited;
+	int dist = dijkstraPath(vOne, vTwo, visited, adjacency);
+	cout << dist << endl;
+    }
     else if (strcmp(input, "QUIT") == 0) {
       quit = true;
     }
   } while (quit != true);
   return 0;
 }
+
+int dijkstraPath(int source, int destination, vector<int> &visited, int adjacency[][20]) {
+  cout << "starting" << endl;
+  queue* curr = new queue(source, 0);
+  vector<queue*> priorityQueue;
+  cout << "loop" << endl;
+  while (curr->getVertice() != destination) {
+    int vert = curr->getVertice();
+    cout << vert << endl;
+    for (int i = 0; i < 20; i++) {
+       if (adjacency[vert - 1][i] != 0) {
+	 queue* newQueue = new queue(i+1, curr->getDistance() + adjacency[vert][i]);
+	 cout << "Adding!" << newQueue->getDistance() << endl;
+	 priorityQueue.push_back(newQueue);
+       }
+    }
+    cout << "here 3" << endl;
+    visited.push_back(curr->getVertice());
+    for (int i = 0; i < priorityQueue.size(); i++) {
+      if (priorityQueue[i] == curr) {
+	priorityQueue.erase(priorityQueue.begin() + i);
+	break;
+      }
+    }
+    cout << "here 4" << endl;
+    sort(priorityQueue.begin(), priorityQueue.end());
+    cout << priorityQueue[0]->getVertice() << endl;
+    curr = priorityQueue.back();
+    cout << "here 6" << endl;
+  }
+  return priorityQueue.back()->getDistance();
+}
+
